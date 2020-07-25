4ecc15531292b69ac66f9a579a0fc5e191b2b63a
DJ Clarke <denviljclarke@gmail.com>
2020-07-24 19:43:09 -0500

    add homescreen photo & text

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
+import SVG from "../images/undraw_web_developer_p3e5.svg"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -13,15 +14,14 @@ const IndexPage = () => (
       <br />
       Denvil J. Clarke
     </h1>
-    <h2 className="text-gray-900 text-3xl font-medium title-font mb-5">
-      A Software Developer
+    <h2 className="text-gray-900 text-3xl font-medium title-font">
+      I build digital products
     </h2>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      <img src={SVG} alt="" />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    {/* <Link to="/page-2/">Go to page 2</Link> <br />
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
   </Layout>
 )
 
