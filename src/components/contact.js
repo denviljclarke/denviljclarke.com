07622a067bc136ca16a3652d8a17c0a3d799b97d
DJ Clarke <denviljclarke@gmail.com>
2020-08-01 00:36:34 -0500

    added netlify form code

--- src/components/contact.js
+++ src/components/contact.js
@@ -21,12 +21,17 @@ const Contact = () => {
           </p> */}
         </div>
         <div className="lg:w-1/2 md:w-2/3 mx-auto">
-          <div className="flex flex-wrap -m-2">
+          <form
+            method="POST"
+            data-netlify="true"
+            className="flex flex-wrap -m-2"
+          >
             <div className="p-2 w-1/2">
               <input
                 className="w-full bg-gray-100 rounded-sm border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2"
                 placeholder="Name"
                 type="text"
+                name="name"
               />
             </div>
             <div className="p-2 w-1/2">
@@ -34,16 +39,21 @@ const Contact = () => {
                 className="w-full bg-gray-100 rounded-sm border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2"
                 placeholder="Email"
                 type="email"
+                name="email"
               />
             </div>
             <div className="p-2 w-full">
               <textarea
                 className="w-full bg-gray-100 rounded-sm border border-gray-400 focus:outline-none h-48 focus:border-indigo-500 text-base px-4 py-2 resize-none block"
                 placeholder="Message"
+                name="message"
               ></textarea>
             </div>
             <div className="p-2 w-full">
-              <button className="flex mx-auto text-white bg-gray-900 border-0 py-2 px-8 focus:outline-none hover:bg-gray-700 rounded-sm text-lg">
+              <button
+                type="submit"
+                className="flex mx-auto text-white bg-gray-900 border-0 py-2 px-8 focus:outline-none hover:bg-gray-700 rounded-sm text-lg"
+              >
                 Send
               </button>
             </div>
@@ -119,7 +129,7 @@ const Contact = () => {
                 </a> */}
               </span>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </section>
